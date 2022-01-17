970d68518539fdf5577e4ab36149164b434e645d
Костя Ливчак <prayDEATH@mail.ru>
2022-01-17 11:51:27 +0300

    Добавлена карта

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GMap.NET;
+using GMap.NET.MapProviders;
 
 namespace dektopCS
 {
@@ -23,6 +25,32 @@ namespace dektopCS
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            mapView.MapProvider = GoogleMapProvider.Instance;
+            mapView.MinZoom = 1;
+            mapView.MaxZoom = 16;
+            mapView.Zoom = 10;
+            mapView.Position = new GMap.NET.PointLatLng(55.796127, 49.106414);
+            mapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            mapView.CanDragMap = true;
+            mapView.DragButton = MouseButton.Left;
+            mapView.ShowCenter = false;
+            mapView.ShowTileGridLines = false;
+        }
+
+        private void Push(object sender, RoutedEventArgs e)
+        {
+            // Show message box when button is clicked.
+            MessageBox.Show("Поздраляю, вы умеете нажимать кнопку =)");
+            Close();
+        }
+
+        private void CheckForcMeans(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
