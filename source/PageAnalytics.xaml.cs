a10454aaebf4b751aa6b8e4482460f2dd8a6fd0d
Костя Ливчак <prayDEATH@mail.ru>
2022-02-02 18:30:33 +0300

    Добавлена возможность запуска аналитических программ

--- source/PageAnalytics.xaml.cs
+++ source/PageAnalytics.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,14 @@ namespace dektopCS.source
     /// </summary>
     public partial class PageAnalytics : Page
     {
+        desktopDBEntities1 db;
+        string path = @".\data\Analityc\";
         public PageAnalytics()
         {
             InitializeComponent();
+            db = new desktopDBEntities1();
+            db.Analytic.Load();
+            lb.ItemsSource = db.Analytic.Select(a => a.AnalyticFile).ToList();
         }
         private void Back_Click(object sender, RoutedEventArgs e)
         {
@@ -34,16 +41,18 @@ namespace dektopCS.source
             if (lb.SelectedIndex!=-1)
             {
                 MessageBox.Show("\nЗапускается программа - " + lb.SelectedValue);
+                string fName = lb.SelectedValue.ToString();
+                Process.Start(@".\data\Analityc\"+fName+".exe");
             }
 
         }
 
-        private void Item_Loaded(object sender, RoutedEventArgs e)
+        /*private void Item_Loaded(object sender, RoutedEventArgs e)
         {
             for (int i = 0; i < 10; i++)
             {
                 lb.Items.Add("Программа №" + (i + 1));
             }
-        }
+        }*/
     }
 }
